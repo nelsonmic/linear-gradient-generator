f4e7ca392a8f1c29bf6aa634ca8545cc28bd45b4
unknown <nelsonmichael4real@gmail.com>
2020-07-29 21:03:22 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var h3 = document.querySelector("h3");
+var input1 = document.querySelector(".color1");
+var input2 = document.querySelector(".color2");
+var body = document.querySelector("#gradient");
+
+
+function gradientGenerator(){
+    body.style.background = "linear-gradient(to right, " +input1.value+ ", " +input2.value+")";
+    h3.textContent = body.style.background +";";
+}
+
+
+input1.addEventListener("input", gradientGenerator);
+
+input2.addEventListener("input", gradientGenerator);
+

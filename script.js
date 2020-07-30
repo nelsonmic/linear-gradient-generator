3e3ace1bb35605e885fffbfad48bbe3255202051
unknown <nelsonmichael4real@gmail.com>
2020-07-30 10:52:12 -0700

    added new feature for setting direction

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var h3 = document.querySelector("h3");
+var direction = document.querySelector(".direction");
 var input1 = document.querySelector(".color1");
 var input2 = document.querySelector(".color2");
 var body = document.querySelector("#gradient");
 
 
 function gradientGenerator(){
-    body.style.background = "linear-gradient(to right, " +input1.value+ ", " +input2.value+")";
-    h3.textContent = body.style.background +";";
+    body.style.background = "linear-gradient(" +direction.value+", " +input1.value+ ", " +input2.value+")";
+    h3.textContent = body.style.background + ";";
 }
 
 
@@ -14,3 +15,4 @@ input1.addEventListener("input", gradientGenerator);
 
 input2.addEventListener("input", gradientGenerator);
 
+direction.addEventListener("input", gradientGenerator);
